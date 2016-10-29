a16a7fa10d5e2986866f5ec6eb7025c853e0b81e
wuyue <yue10owen@gmail.com>
2016-10-29 14:00:28 -0500

    first
    first

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cs707project
+//
+//  Created by wuyue on 10/29/16.
+//  Copyright © 2016 cs707. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
